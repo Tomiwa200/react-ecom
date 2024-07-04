9e3527ff927397aa5bd3741416280f39605078e1
Tomiwa <bankolefemi09@gmail.com>
2024-07-04 14:30:00 +0100

    npm

---
+++ build/precache-manifest.94a470c8f0873467dced0ed66cff2c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856bee10f20cbfcce046ac1381ad0c17",
+    "url": "/react-ecom/index.html"
+  },
+  {
+    "revision": "c8f67e45ba43c8f050e3",
+    "url": "/react-ecom/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "085dcda788fb52ca6154",
+    "url": "/react-ecom/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c8f67e45ba43c8f050e3",
+    "url": "/react-ecom/static/js/2.c580cb03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ecom/static/js/2.c580cb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "085dcda788fb52ca6154",
+    "url": "/react-ecom/static/js/main.3c38c63b.chunk.js"
+  },
+  {
+    "revision": "1d2054e325d1e9fcf565",
+    "url": "/react-ecom/static/js/runtime-main.ad3a833e.js"
+  }
+]);
